7a77d58c7241f8b7bebec3976eaf44b17108e81e
jszum <jaroslaw.szumega@gmail.com>
2015-07-03 10:17:45 +0200

    Python scripts that reads data from csv and converts them into 8 *.wav files

---
+++ converter.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import sys
+import wave
+import array
+
+def convert(filename):
+    csv = open(filename, 'r')
+    data = csv.read().splitlines()
+
+    rawdata = []
+    for i in range(8):
+        rawdata.append( array.array('h') )
+
+    for line in data:
+        if line:
+            fields = line.split(';')
+            for i in range(8):
+                rawdata[i].append( int(fields[i]) )
+
+    return rawdata
+
+
+def create_wave(wavename, data):
+    wave_object = wave.open(wavename, 'wb')
+
+    nchannels = 1
+    sampwidth = 2
+    framerate = 192000
+    nframes = 0
+    comptype = 'NONE'
+    compname = 0
+
+    params = (nchannels, sampwidth, framerate, nframes, comptype, compname)
+    # print(params)
+
+    wave_object.setparams(params)
+    wave_object.writeframes( data.tostring() )
+    wave_object.close()
+
+
+def main(csvfile):
+    data = convert(csvfile)
+
+    for i in range(8):
+        create_wave(csvfile[:-4] + str(i) + '.wav', data[i])
+
+
+if __name__ == '__main__':
+    print( "Path: " + ''.join(sys.argv) )
+    main(sys.argv[1])
